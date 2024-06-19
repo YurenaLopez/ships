61a7b452520241db241d21e48ddb3ada11f20f81
YurenaLopez <7836067@alumnos.ufv.es>
2024-06-19 22:10:58 +0200

    first commit

---
+++ Backend/src/main/java/es/ufv/dis/final2024/yls/YlsApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.yls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlsApplication.class, args);
+	}
+
+}
